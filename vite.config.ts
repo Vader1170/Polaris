839ae09b1bb996d8215c115120d02b916407e20a
Vader1170 <sourish.balaji@gmail.com>
2026-07-22 12:03:32 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,11 +16,7 @@ build: {
   rollupOptions: {
     input: {
       main: path.resolve(__dirname, 'index.html'),
-      mission: path.resolve(__dirname, 'mission.html'),
-      principles: path.resolve(__dirname, 'principles.html'),
-      howitworks: path.resolve(__dirname, 'how-it-works.html'),
       navigator: path.resolve(__dirname, 'navigator.html'),
-      story: path.resolve(__dirname, 'story.html'),
       signin: path.resolve(__dirname, 'signin.html'),
       about: path.resolve(__dirname, 'about.html'),
     },
